e9b2689b58ef2056c7bd66cef0423af8e08ce551
Pranoy M <pranoym101@gmail.com>
2026-05-15 02:09:21 -0400

    Task 4

---
+++ src/main/java/com/jpmc/midascore/component/IncentiveClient.java
@@ -0,0 +1,22 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.foundation.Incentive;
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class IncentiveClient {
+    private static final String INCENTIVE_URL = "http://localhost:8080/incentive";
+
+    private final RestTemplate restTemplate;
+
+    public IncentiveClient(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+    }
+
+    public Incentive getIncentive(Transaction transaction) {
+        return restTemplate.postForObject(INCENTIVE_URL, transaction, Incentive.class);
+    }
+}
--- src/main/java/com/jpmc/midascore/component/TransactionProcessor.java
+++ src/main/java/com/jpmc/midascore/component/TransactionProcessor.java
@@ -2,6 +2,7 @@ package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Incentive;
 import com.jpmc.midascore.foundation.Transaction;
 import com.jpmc.midascore.repository.TransactionRecordRepository;
 import com.jpmc.midascore.repository.UserRepository;
@@ -12,10 +13,12 @@ import org.springframework.stereotype.Component;
 public class TransactionProcessor {
     private final UserRepository userRepository;
     private final TransactionRecordRepository transactionRecordRepository;
+    private final IncentiveClient incentiveClient;
 
-    public TransactionProcessor(UserRepository userRepository, TransactionRecordRepository transactionRecordRepository) {
+    public TransactionProcessor(UserRepository userRepository, TransactionRecordRepository transactionRecordRepository, IncentiveClient incentiveClient) {
         this.userRepository = userRepository;
         this.transactionRecordRepository = transactionRecordRepository;
+        this.incentiveClient = incentiveClient;
     }
 
     @Transactional
@@ -27,11 +30,14 @@ public class TransactionProcessor {
             return;
         }
 
+        Incentive incentive = incentiveClient.getIncentive(transaction);
+        float incentiveAmount = incentive == null ? 0 : incentive.getAmount();
+
         sender.setBalance(sender.getBalance() - transaction.getAmount());
-        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount() + incentiveAmount);
 
         userRepository.save(sender);
         userRepository.save(recipient);
-        transactionRecordRepository.save(new TransactionRecord(sender, recipient, transaction.getAmount()));
+        transactionRecordRepository.save(new TransactionRecord(sender, recipient, transaction.getAmount(), incentiveAmount));
     }
 }
